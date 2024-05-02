ff0b8f09add00e825c27900eff9940acd059902e
Robertarezza <roberta.rezza@gmail.com>
2024-05-02 12:58:45 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script>
+
+import AppBanner from "./components/AppBanner.vue"
+
+export default {
+  components: {
+
+    AppBanner
+  
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template>
+<h1>Hello</h1>
+    <AppBanner />
+    <div class="banner">
+      <h2>ciao</h2>
+        
+    </div>
+  
+ 
+</template>
+
+<style lang="scss">
+
+
+</style>
---
+++ src/components/AppBanner.vue
@@ -0,0 +1,29 @@
+<script >
+export default {
+  data () {
+    return {
+        title: "ciao",
+        imageName:"vue.svg",
+    }
+  },
+  methods: {
+      getImagePath(imageName) {
+        return new URL(`../assets/${imageName}`, import.meta.url).href;
+      },
+    },
+
+}
+</script>
+
+<template>
+  <h1>{{title}}</h1>
+  <img :src="getImagePath(imageName)" alt="" />
+
+</template>
+
+<style scoped>
+h1{
+    color: red;
+    
+}
+</style>
